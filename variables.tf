e584e31faa70ce528d351e6b8f91a1f342d20020
Matt Scafe <matt.scafe@rackspace.com>
2019-03-08 10:49:32 -0600

    Updated Variables to match new naming scheme

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "vnet_name" {
   description = "Name of the vnet to create"
-  default     = "acctvnet"
+  default     = "vnet-eus-terraform"
 }
 
 variable "resource_group_name" {
   description = "Default resource group name that the network will be created in."
-  default     = "myapp-rg"
+  default     = "rg-eus-mscafe-terraform"
 }
 
 variable "location" {
@@ -14,7 +14,7 @@ variable "location" {
 
 variable "address_space" {
   description = "The address space that is used by the virtual network."
-  default     = "10.0.0.0/16"
+  default     = "10.20.0.0/22"
 }
 
 # If no values specified, this defaults to Azure DNS 
@@ -25,12 +25,12 @@ variable "dns_servers" {
 
 variable "subnet_prefixes" {
   description = "The address prefix to use for the subnet."
-  default     = ["10.0.1.0/24"]
+  default     = ["10.20.1.0/24"]
 }
 
 variable "subnet_names" {
   description = "A list of public subnets inside the vNet."
-  default     = ["subnet1"]
+  default     = ["sn-eus-internal"]
 }
 
 variable "tags" {
